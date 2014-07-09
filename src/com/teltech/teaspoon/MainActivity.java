8f08002c958b6a86b1ade9ca03053531341218bf
Alan Beebe <alan.beebe@gmail.com>
2014-07-09 16:36:21 -0400

    Initial Commit

---
+++ src/com/teltech/teaspoon/MainActivity.java
@@ -0,0 +1,32 @@
+package com.teltech.teaspoon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    }
+    
+    @Override   
+    public void onStart() {
+       super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+       super.onStop();
+    }
+    
+    @Override
+    public void onPause() {
+       super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+    	super.onResume();
+    }
+}
